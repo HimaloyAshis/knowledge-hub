e7013f07a260ae9846ad539f4cdf9f70829c769f
himaloyash <himaloyashis98@gmail.com>
2023-09-22 13:40:01 +0600

    knowledge

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -14,12 +14,12 @@ const Card = ({Blog}) => {
             {
                 Blog.map(blog=>{
                     
-                    if(blog.Blog_title===blog.Blog_title){
+                    if(blog.Blog_title!==blog.Blog_title){
+                        
                         return <h4 className='mt-4 shadow-sm p-3 mb-5 bg-body-tertiary rounded'>{blog.Blog_title} !!</h4>
                     }
                     else{
-                        return  console.log('not cool')
-
+                        
                     }
                 })
             }
